ff0975e9ab61a1aff4387552263a66a481efb89e
Vipul <sharma2702vipul@gmail.com>
2023-04-17 19:41:06 +0530

    Second commit

--- Registration/settings.py
+++ Registration/settings.py
@@ -10,11 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+# import os
 from pathlib import Path
 from . info import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 EMAIL_USE_TLS =EMAIL_USE_TLS
 EMAIL_HOST = EMAIL_HOST
@@ -123,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# MEDIA_URL = 'images/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- authentication/views.py
+++ authentication/views.py
@@ -22,6 +22,7 @@ def home(request):
 def signup(request):
 
     if request.method == "POST":
+        
         # username = request.POST.get('username')
         username = request.POST.get('username')
         fname = request.POST.get('fname')
@@ -60,7 +61,7 @@ def signup(request):
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
-        # myuser.is_active = False
+        myuser.is_active = True
 
         myuser.save()
 
